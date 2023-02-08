b493745613044beb0307abcfb934313ed2bf25c3
jisu2000 <spjisu2000@gmail.com>
2023-02-08 23:31:13 +0530

    Modified

--- src/main/java/org/jisu/App.java
+++ src/main/java/org/jisu/App.java
@@ -5,58 +5,70 @@ import org.jisu.service.CustomerService;
 import org.jisu.service.CustomerServiceImpl;
 
 import java.util.Scanner;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) {
         CustomerService service = new CustomerServiceImpl();
         Scanner sc = new Scanner(System.in);
-
-        int choice;
+        int choose;
         do {
-            
-            System.out.println("Enter |1|-> (Add Customer) |2|-> (Close Account) |3|-> (Display a customer)  |4|-> (Display all customer)");
-            System.out.println("|5|->(Deposit) |6|-> (Withdraw) |7|->(Balance Inquiry) |8|-> (Get Statement) |9-> (Neft) |0|->(Exit)");
-            int id;
-            Customer temp;
-            Customer temp2;
-            choice = sc.nextInt();
+            System.out.println("Enter [1] for do Admin Work || Enter [2] for do Customer Work || [-1] for exit");
+            choose = sc.nextInt();
+
+            switch (choose) {
+                case 1:
+                    Admin();
+                    break;
+
+                case 2:
+                    User();
+                    break;
+            }
+        } while(choose!=-1);
+    }
+
+     public static void Admin() {
+        int id;
+        int choice;
+        Scanner scan=new Scanner(System.in);
+        CustomerService service = new CustomerServiceImpl();
+        Customer temp;
+
+        do {
+            System.out.println("Enter |1|-> (Add Customer) |2|-> (Close Account) |3|-> (Display a customer)  |4|-> (Display all customer) |-1|=> to exit");
+            choice =scan.nextInt();
             switch (choice) {
                 case 1:
                     temp = new Customer();
                     System.out.println("Enter Customer Details");
                     System.out.println("Enter Customer Id");
-                    temp.setCustomerid(sc.nextInt());
+                    temp.setCustomerid(scan.nextInt());
                     if (service.Find(temp.getCustomerid()) != null) {
                         System.out.println("Customer Id is already present");
                         break;
                     }
                     System.out.println("Enter Customer name");
-                    temp.setCustomerName(sc.next());
+                    temp.setCustomerName(scan.next());
                     System.out.println("Enter Mobile No");
-                    temp.setMobileNo(sc.next());
+                    temp.setMobileNo(scan.next());
                     System.out.println("Enter Opening Balance");
-                    temp.setBalance(sc.nextInt());
+                    temp.setBalance(scan.nextInt());
 
 
                     service.addCustomer(temp);
-                    System.out.println("Congrats !! "+temp.getCustomerName()+" Welcome to Our bank");
+                    System.out.println("Congrats !! " + temp.getCustomerName() + " Welcome to Our bank");
                     break;
 
                 case 2:
                     System.out.println("Enter CustomerId");
-                     id = sc.nextInt();
+                    id = scan.nextInt();
                     if (service.Find(id) == null) {
                         System.out.println("Customer is not present");
                     } else {
                         temp = service.Find(id);
-                        String t=temp.getCustomerName();
+                        String t = temp.getCustomerName();
                         service.deleteCustomer(id);
-                        System.out.println(" Hi! "+t+" we have closed Your account You were our valuable customer");
+                        System.out.println(" Hi! " + t + " we have closed Your account You were our valuable customer");
 
 
                     }
@@ -64,7 +76,7 @@ public class App
 
                 case 3:
                     System.out.println("Enter Customer id");
-                    id = sc.nextInt();
+                    id = scan.nextInt();
                     temp = service.Find(id);
                 {
                     if (temp == null) {
@@ -83,110 +95,111 @@ public class App
                     break;
 
 
-                case 0:
-                    break;
+            }
 
+        }while (choice!=-1);
+    }
+
+
+    public static void User()
+    {
+        int choice;
+        int id;
+        Scanner sc=new Scanner(System.in);
+        CustomerService service = new CustomerServiceImpl();
+        Customer temp;
+        Customer temp2;
+        do {
+            System.out.println("|5|->(Deposit) |6|-> (Withdraw) |7|->(Balance Inquiry) |8|-> (Get Statement) |9-> (Neft) |0|->(Exit)");
+            choice = sc.nextInt();
+            switch (choice) {
                 case 5:
                     System.out.println("Enter the Customerid ");
-                    id=sc.nextInt();
-                    if(service.Find(id)==null)
-                    {
+                    id = sc.nextInt();
+                    if (service.Find(id) == null) {
                         System.out.println("Invalid CustomerID");
                         break;
-                    }
-                    else {
-                        temp=service.Find(id);
+                    } else {
+                        temp = service.Find(id);
                         System.out.println("Enter the amount to deposit");
-                        int prev=temp.getBalance();
-                        int bal=sc.nextInt();
-                        service.AddBalance(temp,bal);
-                        System.out.println("Account Credited with Rs "+bal+"/- Available amount Rs "+(prev+bal)+"/-");
+                        int prev = temp.getBalance();
+                        int bal = sc.nextInt();
+                        service.AddBalance(temp, bal);
+                        System.out.println("Account Credited with Rs " + bal + "/- Available amount Rs " + (prev + bal) + "/-");
 
                     }
                     break;
 
                 case 6:
                     System.out.println("Enter Customer id");
-                    id= sc.nextInt();
-                    if(service.Find(id)==null)
-                    {
+                    id = sc.nextInt();
+                    if (service.Find(id) == null) {
                         System.out.println("Customer not found!!");
                         break;
                     }
                     System.out.println("Enter Amount to Withdrawal");
-                    int amount=sc.nextInt();
-                    temp=service.Find(id);
-                    if(amount> temp.getBalance())
-                    {
+                    int amount = sc.nextInt();
+                    temp = service.Find(id);
+                    if (amount > temp.getBalance()) {
                         System.out.println("Insufficient Balance !!");
-                    }
-                    else {
-                        int bal= temp.getBalance()-amount;
-                        service.WithDraw(temp,amount);
+                    } else {
+                        int bal = temp.getBalance() - amount;
+                        service.WithDraw(temp, amount);
 
-                        System.out.println("Account has been debited with Rs "+amount+"/- Available amount Rs "+bal+"/-");
+                        System.out.println("Account has been debited with Rs " + amount + "/- Available amount Rs " + bal + "/-");
                     }
                     break;
 
                 case 7:
                     System.out.println("Enter Customer Id");
-                    id= sc.nextInt();
-                    if(service.Find(id)==null)
-                    {
+                    id = sc.nextInt();
+                    if (service.Find(id) == null) {
                         System.out.println("Customer not found !!");
-                    }
-                    else {
-                        temp=service.Find(id);
-                        System.out.println("Available amount Rs "+temp.getBalance()+"/-");
+                    } else {
+                        temp = service.Find(id);
+                        System.out.println("Available amount Rs " + temp.getBalance() + "/-");
                     }
                     break;
 
                 case 8:
                     System.out.println("Enter Customer id");
-                    id= sc.nextInt();
+                    id = sc.nextInt();
                     service.getSt(id).forEach(System.out::println);
                     break;
 
                 case 9:
                     System.out.println("Enter Sender CustomerId");
-                    id=sc.nextInt();
-                    if(service.Find(id)==null)
-                    {
+                    id = sc.nextInt();
+                    if (service.Find(id) == null) {
                         System.out.println("CustomerId not found!");
                         break;
                     }
 
                     System.out.println("Enter Receiver CustomerId");
-                    int id2=sc.nextInt();
-                    if(service.Find(id2)==null)
-                    {
+                    int id2 = sc.nextInt();
+                    if (service.Find(id2) == null) {
                         System.out.println("Customer Not found");
                         break;
                     }
 
                     System.out.println("Enter the Amount for Transfer");
-                    int n=sc.nextInt();
-                    temp=service.Find(id);
-                    temp2=service.Find(id2);
+                    int n = sc.nextInt();
+                    temp = service.Find(id);
+                    temp2 = service.Find(id2);
 
-                    if(temp.getBalance()<n)
-                    {
+                    if (temp.getBalance() < n) {
                         System.out.println("Insufficient Balance!!");
                         break;
                     }
-                    service.neft(temp,temp2,n);
+                    service.neft(temp, temp2, n);
 
-                    System.out.println("Successfully Transferred "+n);
+                    System.out.println("Successfully Transferred " + n);
 
-                    System.out.println(" Your Available Balance is "+(temp.getBalance()-n)+"/-");
+                    System.out.println(" Your Available Balance is " + (temp.getBalance() - n) + "/-");
                     break;
 
             }
-
-
-        }while (choice!=0) ;
-
+        }while (choice!=0);
     }
-
-    }
+}
 
